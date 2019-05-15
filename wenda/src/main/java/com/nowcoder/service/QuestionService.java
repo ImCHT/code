395f5e79d590a10fd7fc456339e0c74d1fd22b69
zhoulou <765917163@qq.com>
2019-05-15 21:58:28 +0800

    高级5一点

--- wenda/src/main/java/com/nowcoder/service/QuestionService.java
+++ wenda/src/main/java/com/nowcoder/service/QuestionService.java
@@ -16,4 +16,9 @@ public class QuestionService {
     public List<Question> getLatestQuestions(int userId, int offset, int limit) {
         return questionDAO.selectLatestQuestions(userId, offset, limit);
     }
+
+    public int addQuestion(Question question){
+        //需要做敏感词过滤
+        return questionDAO.addQuestion(question)>0?question.getId():0;
+    }
 }
